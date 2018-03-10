7ff882185c513034f3a0b4cd77720472652108cd
Khaleel Younis <khaleelyounis1@gmail.com>
2018-03-09 16:15:09 -0800

    added portfolio modals

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,6 +10,7 @@ $(function () {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
+      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -23,6 +24,7 @@ $(function () {
         type: "POST",
         data: {
           name: name,
+          phone: phone,
           email: email,
           message: message
         },
